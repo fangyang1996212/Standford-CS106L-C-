0aa85a6b3b0372d22479cc709774483cf5258ab8
fangyang1996212 <37264455+fangyang1996212@users.noreply.github.com>
2020-07-04 22:25:47 +0800

    Update main2.cpp

--- Chapter 5: STL Sequence Containers/用vector排序输入的整数/main2.cpp
+++ Chapter 5: STL Sequence Containers/用vector排序输入的整数/main2.cpp
@@ -4,23 +4,27 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include "function_declare.h"
+#include "function.hpp"
 using namespace std;
 
-int main() {
+string GetLine();
+int GetInteger();
+size_t InsertionIndex(vector<int>& v, int toInsert);
 
-   vector<int> values;
-   
-   /* Read the values. */
-   for (int i = 0; i < kNumValues; ++i) {
-      cout << "Enter an integer: ";
-      int val = GetInteger();
-     
-     /* Insert the element at the correct position. */
-      values.insert(values.begin() + InsertionIndex(values, val), val);
-   }
-   
-   /* Print out the sorted list. */
-   for (size_t i = 0; i < values.size(); ++i)
-        cout << values[i] << endl;
+const int kNumValues = 10;
+
+int main() {
+     vector<int> values;
+     /* Read the values. */
+     for (int i = 0; i < kNumValues; ++i) {
+         cout << "Enter an integer: ";
+         int val = GetInteger();
+         
+         /* Insert the element at the correct position. */
+        values.insert(values.begin() + InsertionIndex(values, val), val);
+     }
+     /* Print out the sorted list. */
+     for (size_t i = 0; i < values.size(); ++i)
+            cout << values[i] << endl;
  }
+
