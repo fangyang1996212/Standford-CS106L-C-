c1ec19572df46c87bb328d622c15c4d96c911128
fangyang1996212 <37264455+fangyang1996212@users.noreply.github.com>
2020-07-04 21:49:25 +0800

    Create function_implement.cpp

---
+++ Chapter 5: STL Sequence Containers/用vector排序输入的整数/function_implement.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+
+string GetLine() {
+    string result;
+    getline(cin, result);
+    return result;
+}
+
+int GetInteger() {
+    while(true) { // Read input until user enters valid data
+    stringstream converter;
+    converter << GetLine();
+    /* Try reading an int, continue if we succeeded. */
+    int result;
+    if(converter >> result) {
+        char remaining;
+        if(converter >> remaining) // Something's left, input is invalid
+            cout << "Unexpected character: " << remaining << endl;
+        else
+            return result;
+    } else
+        cout << "Please enter an integer." << endl;
+        
+        cout << "Retry: ";
+    }
+}
+
+size_t GetSmallestIndex(vector<int>& v, size_t startIndex) {
+        size_t bestIndex = startIndex;
+        for (size_t i = startIndex; i < v.size(); ++i)
+            if (v[i] < v[bestIndex])
+                bestIndex = i;
+    
+      return bestIndex;
+ }
+
+void SelectionSort(vector<int>& v) {
+       for (size_t i = 0; i < v.size(); ++i) {
+            size_t smallestIndex = GetSmallestIndex(v, i); // We'll write this
+                                                           // function momentarily
+           
+            swap (v[i], v[smallestIndex]);
+       }
+}
+
