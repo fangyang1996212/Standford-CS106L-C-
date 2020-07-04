8a3252a9cc7795d930aa1c134b408809ea9e8601
fangyang1996212 <37264455+fangyang1996212@users.noreply.github.com>
2020-07-04 21:45:50 +0800

    Create function_declare.h

---
+++ Chapter 5: STL Sequence Containers/用vector排序输入的整数/function_declare.h
@@ -0,0 +1,13 @@
+
+
+#ifndef function_declare_hpp
+#define function_declare_hpp
+
+
+
+string GetLine();
+int GetInteger();
+size_t GetSmallestIndex(vector<int>& v, size_t startIndex);
+SelectionSort(vector<int>& v);
+
+#endif /* strutils_hpp */
