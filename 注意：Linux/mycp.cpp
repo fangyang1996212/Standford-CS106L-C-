93d8bd9b3606c873d8aaffdd6a7f4586cf06d0cf
fangyang1996212 <37264455+fangyang1996212@users.noreply.github.com>
2020-09-05 11:07:42 +0800

    Update and rename Notes：Linux/mycp.cpp to 注意：Linux/mycp.cpp

--- 注意：Linux/mycp.cpp
+++ 注意：Linux/mycp.cpp
@@ -5,8 +5,8 @@ int main(int argc, char* argv[])
 
 	int fd1 = open(argv[1], O_RDONLY);
   
-	// O_TRUNC: 覆盖有内容的文件  0664:permission
-  // O_CREAT：如果文件不存在，则创造出一个新的
+         // O_TRUNC: 覆盖有内容的文件  0664:permission
+        // O_CREAT：如果文件不存在，则创造出一个新的
 	int fd2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
 
 	while((n=read(fd1, buf, 1024)) != 0) // 由于可能一次性读不完，所以要用循环
