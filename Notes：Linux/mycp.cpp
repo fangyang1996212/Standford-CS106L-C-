27960e9fbf8001d1d723e6baa4e99175dc9d41ba
fangyang1996212 <37264455+fangyang1996212@users.noreply.github.com>
2020-09-05 11:06:36 +0800

    Create mycp.cpp
    使用 open函数、read函数、write 函数 实现 linux 的 cp 命令，进一步理解read 和 write 函数。
    
    具体实现：
    第一步：创造一个 cp1.txt    cp2.txt
    第二步：g++ mycp.cpp -o mycp 生成一个mycp可执行文件
    第三步：./mycp cp1txt cp2.txt 把cp1 的内容复制给 cp2

---
+++ Notes：Linux/mycp.cpp
@@ -0,0 +1,21 @@
+int main(int argc, char* argv[])
+{
+	char buf[1024];
+	int n = 0;
+
+	int fd1 = open(argv[1], O_RDONLY);
+  
+	// O_TRUNC: 覆盖有内容的文件  0664:permission
+  // O_CREAT：如果文件不存在，则创造出一个新的
+	int fd2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+
+	while((n=read(fd1, buf, 1024)) != 0) // 由于可能一次性读不完，所以要用循环
+	{
+		write(fd2, buf, n);
+	}
+
+	close(fd1);
+	close(fd2);
+
+	return 0;
+}
